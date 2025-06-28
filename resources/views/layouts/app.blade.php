b4af76191f1d61b4572be8a9665632be8839204e
Patrick Angel <patrickgerald116@gmail.com>
2025-06-28 16:11:37 +0100

    Adjust schema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,9 @@
 <html lang="en" data-theme="light">
 <head>
     <meta charset="UTF-8">
-    <title>{{ config('app.name', 'CAC Directory') }}</title>
+    {{-- <title>{{ config('app.name', 'CAC Directory') }}</title> --}}
+    <title>Directory - CAC Oke Igbala Ketu</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/logo.png') }}">
 
     <!-- Bootstrap + Custom -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
