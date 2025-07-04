e4c5d2b2d8a36fc7c08c9f69375f260e4eade34f
Patrick Angel <patrickgerald116@gmail.com>
2025-07-04 10:05:31 +0100

    Commit June 4

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,11 +1,12 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,6 +21,15 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            'name' => 'CAC Admin',
+            'email' => 'admin@cacokeigbalaketu.org',
+            'email_verified_at' => now(),
+            'password' => Hash::make('cacpassword@123'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
